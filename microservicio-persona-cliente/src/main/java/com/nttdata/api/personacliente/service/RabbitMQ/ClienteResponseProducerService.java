7ee6a5d6dea785ad92081bb48e87034801c01ed5
aroman9 <ariana.roman0903@gmail.com>
2024-07-29 19:12:19 -0500

    Actualizacion archivos docker y properties

--- microservicio-persona-cliente/src/main/java/com/nttdata/api/personacliente/service/RabbitMQ/ClienteResponseProducerService.java
+++ microservicio-persona-cliente/src/main/java/com/nttdata/api/personacliente/service/RabbitMQ/ClienteResponseProducerService.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-
-
 public class ClienteResponseProducerService {
     @Autowired
     private RabbitTemplate rabbitTemplate;
